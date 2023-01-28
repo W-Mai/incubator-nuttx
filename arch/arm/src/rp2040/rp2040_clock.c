45c1d003e9eb5c0aa56f7b5302b82c93d9c15a50
Peter Bee <bijunda1@xiaomi.com>
2023-01-28 16:46:04 +0800

    boards/rp2040: overclock to 250MHz
    add config setting LCD SPI frequency to 125MHz
    
    Signed-off-by: Peter Bee <bijunda1@xiaomi.com>

--- arch/arm/src/rp2040/rp2040_clock.c
+++ arch/arm/src/rp2040/rp2040_clock.c
@@ -210,7 +210,7 @@ void clocks_init(void)
 
   /* Configure PLLs
    *                   REF     FBDIV VCO     POSTDIV
-   * PLL SYS: 12 / 1 = 12MHz * 125 = 1500MHZ / 6 / 2 = 125MHz
+   * PLL SYS: 12 / 1 = 12MHz * 125 = 1500MHZ / 6 / 1 = 250MHz
    * PLL USB: 12 / 1 = 12MHz * 40  = 480 MHz / 5 / 2 =  48MHz
    */
 
@@ -222,7 +222,7 @@ void clocks_init(void)
          (RP2040_RESETS_RESET_PLL_SYS | RP2040_RESETS_RESET_PLL_USB))
     ;
 
-  rp2040_pll_init(RP2040_PLL_SYS_BASE, 1, 1500 * MHZ, 6, 2);
+  rp2040_pll_init(RP2040_PLL_SYS_BASE, 1, 1500 * MHZ, 6, 1);
   rp2040_pll_init(RP2040_PLL_USB_BASE, 1, 480 * MHZ, 5, 2);
 
   /* Configure clocks */
@@ -235,7 +235,7 @@ void clocks_init(void)
                          BOARD_XOSC_FREQ,
                          BOARD_REF_FREQ);
 
-  /* CLK SYS = PLL SYS (125MHz) / 1 = 125MHz */
+  /* CLK SYS = PLL SYS (250MHz) / 1 = 250MHz */
 
   rp2040_clock_configure(RP2040_CLOCKS_NDX_SYS,
                          RP2040_CLOCKS_CLK_SYS_CTRL_SRC_CLKSRC_CLK_SYS_AUX,
